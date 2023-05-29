b5c2c9d2c0d6dea9f37a44febb90a3757096d67a
krushika Polra <kpolra548@gamil.com>
2023-05-29 06:52:17 -0700

    First commit

---
+++ lib/big_cart/welcome.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [Image.asset("assets/")],
+          )
+        ],
+      ),
+    );
+  }
+}
