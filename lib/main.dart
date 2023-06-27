d6cefe6953e130fa9146b15d2285c6c1b348b481
Unnati Donda <unnatidonda866@gmail.com>
2023-06-27 13:24:33 +0530

    screenchanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'big_cart/order_success.dart';
+import 'big_cart/category_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const OrderSuccess(),
+      home: const CategoryScreen(),
     );
   }
 }
