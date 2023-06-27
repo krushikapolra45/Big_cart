ce73a9c58aa8e2e54a1b322c36e29324dd9d6f0a
Unnati Donda <unnatidonda866@gmail.com>
2023-06-27 14:52:39 +0530

    screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'big_cart/category_screen.dart';
+import 'big_cart/address.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CategoryScreen(),
+      home: const Address(),
     );
   }
 }
